c3eee69badd6b42dff59376aaad45b6a736015c9
daniellammerhirt <daniellammer75@gmail.com>
2026-04-22 13:43:12 -0300

    refactor: muda a implementação da entrada de dados usando a generalização

--- Calculadora/src/operacoes/Soma.java
+++ Calculadora/src/operacoes/Soma.java
@@ -5,6 +5,7 @@
 package operacoes;
 
 import java.util.Scanner;
+import numeros.EntradaNumeros;
 
 /**
  *
@@ -12,9 +13,8 @@ import java.util.Scanner;
  */
 public class Soma {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        System.out.println(x + y);
+        EntradaNumeros entradaNumeros = new EntradaNumeros();
+        entradaNumeros.pedirNumeros();
+        System.out.println(entradaNumeros.x + entradaNumeros.y);
     }
 }
